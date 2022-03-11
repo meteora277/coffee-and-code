1dd91e291d700428ca56612e80102a1bb90bcae7
meteora277 <justin.s.diaz@gmail.com>
2022-03-11 12:56:48 -0500

    create cta component

---
+++ components/Cta.jsx
@@ -0,0 +1,20 @@
+const Cta = () => {
+  return (
+    <article>
+      <h1>Justin Diaz was here</h1>
+
+      <style jsx>{`
+        article {
+          height: 720px;
+          background: salmon;
+        }
+        h1 {
+          margin: 0;
+          color: white;
+        }
+      `}</style>
+    </article>
+  );
+};
+
+export default Cta;
